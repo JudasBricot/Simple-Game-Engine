690062fb4cd1da061c455ff506ba74de571cc6a0
JudasBricot <arthur.wuhrlin@telecom-paris.fr>
2023-03-18 22:56:57 +0100

    Added simple Application Class

--- Thats-the-one/src/Application.cpp
+++ Thats-the-one/src/Application.cpp
@@ -1,24 +1,32 @@
 #include "Application.h"
 
-#include "EventSystem/ApplicationEvent.h"
+#include "Logging/Log.h"
 
-Application::Application()
+namespace Judas_Engine
 {
-	std::string name = "My game";
-
-	m_Window = std::unique_ptr<Window>(Window::Create({ 640, 480, name.c_str()}));
-}
-
-Application::~Application()
-{
-}
-
-void Application::Run()
-{
-	MouseButtonPressedEvent buttonPressed(1);
-	std::cout << buttonPressed.ToString() << std::endl;
-	while (m_Running)
+	Application::Application()
 	{
-		m_Window->Update();
+		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(std::bind(&Application::OnEvent, this, std::placeholders::_1));
+	}
+
+	Application::~Application()
+	{
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		JE_CORE_INFO("{0}", e.ToString());
+	}
+
+	void Application::Run()
+	{
+		Judas_Engine::Log::Init();
+		MouseButtonPressedEvent buttonPressed(1);
+		JE_CORE_INFO(buttonPressed.ToString().c_str());
+		while (m_Running)
+		{
+			m_Window->OnUpdate();
+		}
 	}
 }
--- Thats-the-one/src/Application.h
+++ Thats-the-one/src/Application.h
@@ -3,15 +3,23 @@
 #include <iostream>
 #include "Window.h"
 
-class Application
-{
-public:
-	Application();
-	~Application();
+#include "EventSystem/ApplicationEvent.h"
 
-	void Run();
-private:
-	std::unique_ptr<Window> m_Window;
-	bool m_Running = true;
-};
+
+namespace Judas_Engine
+{
+	class Application
+	{
+	public:
+		Application();
+		~Application();
+
+		void OnEvent(Event& e);
+
+		void Run();
+	private:
+		std::unique_ptr<Window> m_Window;
+		bool m_Running = true;
+	};
+}
 
